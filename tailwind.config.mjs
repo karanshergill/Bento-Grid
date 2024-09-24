d05a0ec11bffb9c7734a2f15d5ff586753f6451f
Karan <email@karanshergill.com>
2024-09-24 12:12:41 +0530

    add css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,14 @@ export default {
 	],
 	theme: {
 		extend: {
+			colors: {
+				'purple-light': 'hsl(254, 88%, 90%)',
+				'purple-dark': 'hsl(256, 67%, 59%)',
+				'yellow-light': 'hsl(31, 66%, 93%)',
+				'yellow-dark': 'hsl(39, 100%, 71%)',
+				'white': 'hsl(0, 0%, 100%)',
+				'black': 'hsl(0, 0%, 7%)'
+			},
 			fontFamily: {
 				'dm-sans': ['DM Sans Variable', 'sans-serif']
 			}
